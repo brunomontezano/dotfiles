76169dd3b207e8d7d1e44d77ca42cc869ec64083
Bruno Montezano <brunodoyt@gmail.com>
2020-07-08 17:54:17 -0300

    Update to nord color scheme.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -4,22 +4,22 @@
 
 config.load_autoconfig()
 
-base00 = "#002b36"
-base01 = "#073642"
-base02 = "#586e75"
-base03 = "#657b83"
-base04 = "#839496"
-base05 = "#93a1a1"
-base06 = "#eee8d5"
-base07 = "#fdf6e3"
-base08 = "#dc322f"
-base09 = "#cb4b16"
-base0A = "#b58900"
-base0B = "#859900"
-base0C = "#2aa198"
-base0D = "#268bd2"
-base0E = "#6c71c4"
-base0F = "#d33682"
+base00 = "#2E3440"
+base01 = "#3B4252"
+base02 = "#434C5E"
+base03 = "#4C566A"
+base04 = "#D8DEE9"
+base05 = "#E5E9F0"
+base06 = "#ECEFF4"
+base07 = "#8FBCBB"
+base08 = "#BF616A"
+base09 = "#D08770"
+base0A = "#EBCB8B"
+base0B = "#A3BE8C"
+base0C = "#88C0D0"
+base0D = "#81A1C1"
+base0E = "#B48EAD"
+base0F = "#5E81AC"
 
 # set qutebrowser colors
 
