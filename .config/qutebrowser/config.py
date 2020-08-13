0d1b26cb1ef4fa742ea0fa48674fab10789b128c
Bruno Montezano <brunodoyt@gmail.com>
2020-08-13 18:31:19 -0300

    Add comments.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -3,6 +3,7 @@ import dracula.draw
 # Load existing settings made via :set
 config.load_autoconfig()
 
+# Call the dracula theme function
 dracula.draw.blood(c, {
     'spacing': {
         'vertical': 6,
