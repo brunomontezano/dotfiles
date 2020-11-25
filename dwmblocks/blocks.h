3a2aff3c64493cc1fc359fd82b916c5d66b2ade2
Bruno Montezano <brunodoyt@gmail.com>
2020-11-25 13:15:00 -0300

    Changing volume and battery order.

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
-    {"",     "battery",         5,                  3},
-
 	{"", "volume",  			0,		            10},
+
+    {"",     "battery",         5,                  3},
 
 	{" 🕑 ", "clock",			60,		            1},
 };
