e59735b341e2707af403be8647412bd0519b2081
Bruno Montezano <brunodoyt@gmail.com>
2020-11-24 23:19:01 -0300

    Removing kernel, uptime and memory blockets.

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,14 +1,6 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
-    {" 🐧 ", "kernel",		    3600,		        2},
-
-	{" 🔺 ", "upt",		        60,		            2},
-
-	{" 📦 ", "pacupdate",		360,		        9},
-	
-	{" 💻 ", "memory",	        10,		            14},
-
     {"",     "battery",         5,                  3},
 
 	{"", "volume",  			0,		            10},
