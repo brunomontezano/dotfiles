5076719fdd8b0ccc65c67eac1096eb63c05205c7
Bruno Montezano <brunodoyt@gmail.com>
2024-03-05 18:29:15 -0300

    Add keymaps for telescope.nvim

--- .config/nvim/lua/bruno/lazy.lua
+++ .config/nvim/lua/bruno/lazy.lua
@@ -268,14 +268,17 @@ local plugins = {
     },
     {
         "nvim-telescope/telescope.nvim",
-	lazy = true,
+        lazy = true,
         config = function()
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
             vim.keymap.set('n', '<leader>fc', builtin.colorscheme, {})
+            vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+            vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, {})
         end
     },
 }
