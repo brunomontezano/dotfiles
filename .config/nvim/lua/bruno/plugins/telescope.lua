c840cf2ab080f2801b69b5b0311f225953cf1d5f
Bruno Montezano <brunodoyt@gmail.com>
2024-06-17 14:30:44 -0300

    Remove LSP telescope commands from telescope file. Add current word telescope search keymap.

--- .config/nvim/lua/bruno/plugins/telescope.lua
+++ .config/nvim/lua/bruno/plugins/telescope.lua
@@ -3,13 +3,12 @@ return {
 	lazy = true,
 	config = function()
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [f]iles"})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind using [g]rep"})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [b]uffers"})
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [h]elp tags"})
-		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [k]eymaps"})
-		vim.keymap.set("n", "<leader>fc", builtin.colorscheme, { desc = "[F]ind [c]olorschemes"})
-		vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "[F]ind [r]eferences"})
-		vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, { desc = "[F]ind [d]efinitions"})
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [f]iles" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind using [g]rep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [b]uffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [h]elp tags" })
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [k]eymaps" })
+		vim.keymap.set("n", "<leader>fc", builtin.colorscheme, { desc = "[F]ind [c]olorschemes" })
+		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [w]ord" })
 	end,
 }
