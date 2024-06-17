a054f920b98a8958537b33f33736b103629eaac4
Bruno Montezano <brunodoyt@gmail.com>
2024-06-17 02:21:54 -0300

    Add trash option for oil.nvim.

--- .config/nvim/lua/bruno/plugins/oil.lua
+++ .config/nvim/lua/bruno/plugins/oil.lua
@@ -5,6 +5,7 @@ return {
 		config = function()
 			require("oil").setup({
 				default_file_explorer = true,
+				delete_to_trash = true,
 			})
 
 			vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
