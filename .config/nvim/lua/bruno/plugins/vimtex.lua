b1f01b08b556872a0e4f2b9ea15061967301a7b9
Bruno Montezano <brunodoyt@gmail.com>
2024-06-17 02:22:24 -0300

    Change back to zathura as default PDF viewer for vimtex.

--- .config/nvim/lua/bruno/plugins/vimtex.lua
+++ .config/nvim/lua/bruno/plugins/vimtex.lua
@@ -2,7 +2,7 @@ return {
 	"lervag/vimtex",
 	ft = "tex",
 	init = function()
-		--vim.g.vimtex_view_method = "zathura"
-		vim.g.vimtex_view_general_viewer = "papers"
+		vim.g.vimtex_view_method = "zathura"
+		-- vim.g.vimtex_view_general_viewer = "papers"
 	end,
 }
