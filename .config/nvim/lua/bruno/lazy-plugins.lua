d70aa24933ea067320c390b6cbbfe2d5ee6d8371
Bruno Montezano <brunodoyt@gmail.com>
2024-07-19 00:11:25 -0300

    refactor: use new syntax of lazy.nvim to call plugin files

--- .config/nvim/lua/bruno/lazy-plugins.lua
+++ .config/nvim/lua/bruno/lazy-plugins.lua
@@ -1,43 +1,7 @@
 require("lazy").setup({
-
-	require("bruno.plugins.plenary"),
-
-	require("bruno.plugins.autopairs"),
-
-	require("bruno.plugins.vimtex"),
-
-	require("bruno.plugins.catppuccin"),
-
-	require("bruno.plugins.todo-comments"),
-
-	require("bruno.plugins.iron"),
-
-	require("bruno.plugins.treesitter"),
-
-	require("bruno.plugins.lsp"),
-
-	require("bruno.plugins.conform"),
-
-	require("bruno.plugins.cmp"),
-
-	require("bruno.plugins.telescope"),
-
-	require("bruno.plugins.debug"),
-
-	require("bruno.plugins.indent-line"),
-
-	require("bruno.plugins.lint"),
-
-	require("bruno.plugins.quarto"),
-
-	require("bruno.plugins.toggle-checkbox"),
-
-	require("bruno.plugins.live-server"),
-
-	require("bruno.plugins.oil"),
-
-	require("bruno.plugins.which-key"),
-}, {
+	spec = {
+		{ import = "bruno.plugins" },
+	},
 	ui = {
 		icons = vim.g.have_nerd_font and {} or {
 			cmd = "📜",
