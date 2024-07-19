cdf3188024562da0a588adff5db067a22c73a839
Bruno Montezano <brunodoyt@gmail.com>
2024-07-19 00:12:33 -0300

    refactor: remove diagnostic-related jumps since i use quicklist now

--- .config/nvim/lua/bruno/remap.lua
+++ .config/nvim/lua/bruno/remap.lua
@@ -12,9 +12,6 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {
 	silent = true,
 })
 
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
