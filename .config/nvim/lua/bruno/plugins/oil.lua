3c1da2780fff1442549f3828a6daf1caee207fb5
Bruno Montezano <brunodoyt@gmail.com>
2024-07-19 00:14:56 -0300

    feat: add trash support and lazy load on keymap

--- .config/nvim/lua/bruno/plugins/oil.lua
+++ .config/nvim/lua/bruno/plugins/oil.lua
@@ -2,13 +2,17 @@ return {
 	{
 		"stevearc/oil.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("oil").setup({
-				default_file_explorer = true,
-				delete_to_trash = true,
-			})
-
-			vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
-		end,
+		keys = {
+			{
+				"-",
+				"<Cmd>Oil<CR>",
+				mode = "n",
+				desc = "Open parent directory",
+			},
+		},
+		opts = {
+			default_file_explorer = true,
+			delete_to_trash = true,
+		},
 	},
 }
