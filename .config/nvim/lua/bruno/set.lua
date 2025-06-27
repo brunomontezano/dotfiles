84d66fc0ee59652741e8fed9f9c251232e9b822a
Bruno Montezano <brunodoyt@gmail.com>
2025-06-27 11:15:08 -0300

    Add autocmd to save state of folds in every file

--- .config/nvim/lua/bruno/set.lua
+++ .config/nvim/lua/bruno/set.lua
@@ -34,3 +34,14 @@ vim.opt.splitbelow = true
 
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+	pattern = { "*.*" },
+	desc = "save view (folds), when closing file",
+	command = "mkview",
+})
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	pattern = { "*.*" },
+	desc = "load view (folds), when opening file",
+	command = "silent! loadview",
+})
